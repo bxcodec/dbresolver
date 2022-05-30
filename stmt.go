1a36a356b83b34cb73003b4805eeaa2113d8b27c
Iman Tumorang <iman.tumorang@gmail.com>
2022-05-30 22:16:44 +0700

    feat: initial commit

---
+++ stmt.go
@@ -0,0 +1,100 @@
+package dbresolver
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Stmt is an aggregate prepared statement.
+// It holds a prepared statement for each underlying physical db.
+type Stmt interface {
+	Close() error
+	Exec(...interface{}) (sql.Result, error)
+	ExecContext(ctx context.Context, args ...interface{}) (sql.Result, error)
+	Query(...interface{}) (*sql.Rows, error)
+	QueryRow(...interface{}) *sql.Row
+}
+
+type stmt struct {
+	db *DB
+
+	rwstmt *sql.Stmt
+	rostmt *sql.Stmt
+}
+
+// Close closes the statement by concurrently closing all underlying
+// statements concurrently, returning the first non nil error.
+func (s *stmt) Close() error {
+	return doParallely(s.db.totalConnection, func(i int) error {
+		if i == 0 {
+			return s.rwstmt.Close()
+		}
+		if s.rostmt != nil {
+			return s.ROStmt().Close()
+		}
+		return nil
+	})
+}
+
+// Exec executes a prepared statement with the given arguments
+// and returns a Result summarizing the effect of the statement.
+// Exec uses the master as the underlying physical db.
+func (s *stmt) Exec(args ...interface{}) (sql.Result, error) {
+	// return s.stmts[0].Exec(args...)
+	return s.RWStmt().Exec(args...)
+}
+
+// ExecContext executes a prepared statement with the given arguments
+// and returns a Result summarizing the effect of the statement.
+// Exec uses the master as the underlying physical db.
+func (s *stmt) ExecContext(ctx context.Context, args ...interface{}) (sql.Result, error) {
+	// return s.stmts[0].Exec(args...)
+	return s.RWStmt().ExecContext(ctx, args...)
+}
+
+// Query executes a prepared query statement with the given
+// arguments and returns the query results as a *sql.Rows.
+// Query uses a slave as the underlying physical db.
+func (s *stmt) Query(args ...interface{}) (*sql.Rows, error) {
+	return s.ROStmt().Query(args...)
+}
+
+// QueryContext executes a prepared query statement with the given
+// arguments and returns the query results as a *sql.Rows.
+// Query uses a slave as the underlying physical db.
+func (s *stmt) QueryContext(ctx context.Context, args ...interface{}) (*sql.Rows, error) {
+	return s.ROStmt().QueryContext(ctx, args...)
+}
+
+// QueryRow executes a prepared query statement with the given arguments.
+// If an error occurs during the execution of the statement, that error
+// will be returned by a call to Scan on the returned *Row, which is always non-nil.
+// If the query selects no rows, the *Row's Scan will return ErrNoRows.
+// Otherwise, the *sql.Row's Scan scans the first selected row and discards the rest.
+// QueryRow uses a slave as the underlying physical db.
+func (s *stmt) QueryRow(args ...interface{}) *sql.Row {
+	return s.ROStmt().QueryRow(args...)
+}
+
+// QueryRowContext executes a prepared query statement with the given arguments.
+// If an error occurs during the execution of the statement, that error
+// will be returned by a call to Scan on the returned *Row, which is always non-nil.
+// If the query selects no rows, the *Row's Scan will return ErrNoRows.
+// Otherwise, the *sql.Row's Scan scans the first selected row and discards the rest.
+// QueryRow uses a slave as the underlying physical db.
+func (s *stmt) QueryRowContext(ctx context.Context, args ...interface{}) *sql.Row {
+	return s.ROStmt().QueryRowContext(ctx, args...)
+}
+
+// ROStmt return the RO statement
+func (s *stmt) ROStmt() *sql.Stmt {
+	if s.rostmt == nil {
+		return s.rwstmt
+	}
+	return s.rostmt
+}
+
+// RWStmt return the RW statement
+func (s *stmt) RWStmt() *sql.Stmt {
+	return s.rwstmt
+}
