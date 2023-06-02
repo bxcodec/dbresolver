c3a1967790013163b61fe85c25c6359adae5bd7b
Hiro <laciferin@gmail.com>
2023-06-03 04:29:45 +0530

    test: get testcases
    Signed-off-by: Hiro <laciferin@gmail.com>

--- fuzz_test.go
+++ fuzz_test.go
@@ -23,6 +23,8 @@ func FuzzMultiWrite(f *testing.F) {
 	}()
 
 	f.Fuzz(func(t *testing.T, wdbCount, rdbCount, lbPolicyID uint8) {
+		t.Fatal()
+
 		policyID := lbPolicyID % uint8(len(LoadBalancerPolicies))
 
 		config := DBConfig{
