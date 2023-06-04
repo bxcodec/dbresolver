25fa0336e0383dcc0647f66747e000fe846c5fcf
Hiro <laciferin@gmail.com>
2023-06-04 06:30:06 +0530

    feat: version.go
    Signed-off-by: Hiro <laciferin@gmail.com>

---
+++ version.go
@@ -0,0 +1,16 @@
+package dbresolver
+
+// Version is the current release version.
+func Version() string {
+	return ""
+}
+
+// Commit: is the HEAD commit of this [Version]
+func Commit() string {
+	return ""
+}
+
+// ReleaseTime is the UTC Time of the release of this [Version]
+func ReleaseTime() string {
+	return ""
+}
