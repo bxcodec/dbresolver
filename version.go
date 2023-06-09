bd725d86ca58227a276b2e3f17fdc9933bcef5c9
Hiro <laciferin@gmail.com>
2023-06-10 02:51:50 +0530

    clear: version.go
    Signed-off-by: Hiro <laciferin@gmail.com>

--- version.go
+++ version.go
@@ -1,10 +1,10 @@
 package dbresolver
 
 // Version is the current release version.
-const Version = "2.1"
+const Version = ""
 
 // Commit: is the HEAD commit of this [Version]
-const Commit = "4d12938cb692ad29618ab3b38b05fc4540948d6a"
+const Commit = ""
 
 // ReleaseTime is the UTC Time of the release of this [Version]
-const ReleaseTime = "2023-06-04T03:34:16Z"
+const ReleaseTime = ""
