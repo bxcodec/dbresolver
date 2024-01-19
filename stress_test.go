e108f594a450dc67cc9f300436c077ad0ce0777b
Nasfame <laciferin@gmail.com>
2024-01-19 09:11:14 +0530

    doc: why test cannot be parallel

--- stress_test.go
+++ stress_test.go
@@ -60,7 +60,7 @@ func TestIssue44(t *testing.T) {
 	for i := 0; i < noOfQueries; i++ {
 
 		t.Run(fmt.Sprintf("q%d", i), func(t *testing.T) {
-			//t.Parallel() //TODO: not concurrent safe
+			//t.Parallel() //TODO: not concurrent safe because of shared mocks
 
 			for _, mock := range allMocks {
 				mock.ExpectQuery(query).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}))
